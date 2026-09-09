6af33511dd9fbb8335a6bc0a009feb53614bb83a
Lorenzo Gaifas <brisvag@gmail.com>
2026-09-09 15:28:07 +0200

    add clipping propagation

--- src/napari/_vispy/visuals/colorbar.py
+++ src/napari/_vispy/visuals/colorbar.py
@@ -123,3 +123,14 @@ class ColorBar(Node):
 
         width, _ = text.get_width_height()
         return width + self.ticks.tick_label_margin, text.get_line_height()
+
+    def _set_clipper(self, node, clipper):
+        """Propagate clipper to child visuals.
+
+        ColorBar is just a Node (not VisualNode) so it doesn't have the filter
+        attach/detach logic; using Compound instead of Node is an alternative
+        that doesn't require these extra tricks, but has other issues (due to
+        our parenting hijacking) which are more complex to solve.
+        """
+        for child in self.children:
+            child._set_clipper(node, clipper)
